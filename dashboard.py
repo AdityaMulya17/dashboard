0486d1da6e82ed94b77f4ccd4c86a3c250a1a946
AdityaMulya17 <129265237+AdityaMulya17@users.noreply.github.com>
2024-06-10 15:39:09 +0700

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -36,72 +36,119 @@ st.header('Kualitas Udara di Kota Changping :sparkles:')
 main_df = changping_df[(changping_df["date"] >= str(start_date)) & 
                 (changping_df["date"] <= str(end_date))]
 
-st.subheader('Kualitas udara tahunan berdasarkan berbagai parameter ')
+st.subheader('Kualitas udara perjam kota Changping  ')
 
-Tahun_df = main_df.groupby("year").mean(numeric_only=True)
+
+Jam_df = changping_df.groupby("hour").mean(numeric_only=True)
+
+fig = plt.figure(figsize=(10,6))
+plt.plot(Jam_df.index, Jam_df["PM2.5"], label="PM2.5")
+plt.plot(Jam_df.index,Jam_df["PM10"], label="PM10")
+plt.plot(Jam_df.index, Jam_df["SO2"], label="SO2")
+plt.plot(Jam_df.index, Jam_df["NO2"], label="NO2")
+plt.plot(Jam_df.index, Jam_df["O3"], label="O3")
+plt.xlabel("Jam")
+plt.ylabel("Konsentrasi Polusi(microgram/m3)")
+plt.legend()
+st.pyplot(fig)
+
+#tren nilai CO
+
+Jam_df = changping_df.groupby("hour").mean(numeric_only=True)
+
+fig = plt.figure(figsize=(10,6))
+plt.plot(Jam_df.index, Jam_df["CO"], label="CO")
+plt.xlabel("Hour")
+plt.ylabel("Konsentrasi Polusi (microgram/m3)")
+plt.legend()
+plt.show()
+st.pyplot(fig)
+
+
+st.subheader('Kualitas udara Perhari Kota Changping ')
+
+Hari_df = changping_df.groupby("day").mean(numeric_only=True)
+
+fig = plt.figure(figsize=(10,6))
+plt.plot(Hari_df.index, Hari_df["PM2.5"], label="PM2.5")
+plt.plot(Hari_df.index, Hari_df["PM10"], label="PM10")
+plt.plot(Hari_df.index, Hari_df["SO2"], label="SO2")
+plt.plot(Hari_df.index, Hari_df["NO2"], label="NO2")
+plt.plot(Hari_df.index, Hari_df["O3"], label="O3")
+plt.xlabel("tanggal")
+plt.ylabel("Konsentrasi Polusi (microgram/m3)")
+plt.legend()
+plt.show()
+st.pyplot(fig)
+
+
+Hari_df = changping_df.groupby("day").mean(numeric_only=True)
+fig = plt.figure(figsize=(10,6))
+plt.plot(Hari_df.index, Hari_df["CO"], label="CO")
+plt.xlabel("tanggal")
+plt.ylabel("Konsentrasi Polusi (microgram/m3)")
+plt.legend()
+plt.show()
+st.pyplot(fig)
+
+st.subheader('Kualitas udara Perbulan Kota Changping ')
+
+Bulan_df = changping_df.groupby("month").mean(numeric_only=True)
+
+fig = plt.figure(figsize=(10,6))
+plt.plot(Bulan_df.index, Bulan_df["PM2.5"], label="PM2.5")
+plt.plot(Bulan_df.index, Bulan_df["PM10"], label="PM10")
+plt.plot(Bulan_df.index, Bulan_df["SO2"], label="SO2")
+plt.plot(Bulan_df.index, Bulan_df["NO2"], label="NO2")
+plt.plot(Bulan_df.index, Bulan_df["O3"], label="O3")
+plt.xlabel("bulan")
+plt.ylabel("Konsentrasi Polusi (microgram/m3)")
+plt.legend()
+plt.show()
+st.pyplot(fig)
+
+Bulan_df = changping_df.groupby("month").mean(numeric_only=True)
+fig = plt.figure(figsize=(10,6))
+plt.plot(Bulan_df.index, Bulan_df["CO"], label="CO")
+plt.xlabel("bulan")
+plt.ylabel("Konsentrasi Polusi (microgram/m3)")
+plt.legend()
+plt.show()
+st.pyplot(fig)
+
+st.subheader('Kualitas udara Pertahun Kota Changping ')
+
+Tahun_df = changping_df.groupby("year").mean(numeric_only=True)
 
 fig = plt.figure(figsize=(10,6))
 plt.plot(Tahun_df.index, Tahun_df["PM2.5"], label="PM2.5")
-plt.xlabel("Tahun")
-plt.ylabel("Konsentrasi Kualitas Udara(microgram/m3)")
-plt.legend()
-st.pyplot(fig)
-
-
-Tahun_df = main_df.groupby("year").mean(numeric_only=True)
-
-fig = plt.figure(figsize=(10,6))
 plt.plot(Tahun_df.index, Tahun_df["PM10"], label="PM10")
-plt.xlabel("Tahun")
-plt.ylabel("Konsentrasi Kualitas Udara(microgram/m3)")
-plt.legend()
-st.pyplot(fig)
-
-
-Tahun_df = main_df.groupby("year").mean(numeric_only=True)
-
-fig = plt.figure(figsize=(10,6))
 plt.plot(Tahun_df.index, Tahun_df["SO2"], label="SO2")
+plt.plot(Tahun_df.index, Tahun_df["NO2"], label="NO2")
+plt.plot(Tahun_df.index, Tahun_df["O3"], label="O3")
 plt.xlabel("Tahun")
-plt.ylabel("Konsentrasi Kualitas Udara(microgram/m3)")
+plt.ylabel("Konsentrasi Polusi (microgram/m3)")
 plt.legend()
+plt.show()
 st.pyplot(fig)
-
-Tahun_df = main_df.groupby("year").mean(numeric_only=True)
 
 fig = plt.figure(figsize=(10,6))
 plt.plot(Tahun_df.index, Tahun_df["CO"], label="CO")
 plt.xlabel("Tahun")
-plt.ylabel("Konsentrasi Kualitas Udara(microgram/m3)")
+plt.ylabel("Konsentrasi Polusi (microgram/m3)")
 plt.legend()
+plt.show()
 st.pyplot(fig)
 
-st.subheader('Kualitas udara PERBULAN berdasarkan berbagai parameter ')
 
 
-Bulan_df = main_df.groupby("month").mean(numeric_only=True)
+st.subheader('Korelasi antar parameter ')
 
-fig = plt.figure(figsize=(10,6))
-plt.plot(Tahun_df.index, Tahun_df["CO"], label="CO")
-plt.xlabel("Bulan")
-plt.ylabel("Konsentrasi Kualitas Udara(microgram/m3)")
-plt.legend()
-st.pyplot(fig)
 
-Bulan_df = main_df.groupby("month").mean(numeric_only=True)
+fig = plt.figure(figsize=(13,9))
 
-fig = plt.figure(figsize=(10,6))
-plt.plot(Tahun_df.index, Tahun_df["PM2.5"], label="PM2.5")
-plt.xlabel("Bulan")
-plt.ylabel("Konsentrasi Kualitas Udara(microgram/m3)")
-plt.legend()
-st.pyplot(fig)
+sns.heatmap(changping_df.corr(numeric_only = True),cmap=plt.cm.Reds,annot=True)
+plt.title("Korelasi Polusi Udara dan Parameter Meteorologi")
+plt.show()
 
-Bulan_df = main_df.groupby("month").mean(numeric_only=True)
-
-fig = plt.figure(figsize=(10,6))
-plt.plot(Tahun_df.index, Tahun_df["PM10"], label="PM10")
-plt.xlabel("Bulan")
-plt.ylabel("Konsentrasi Kualitas Udara(microgram/m3)")
-plt.legend()
 st.pyplot(fig)
